6e7ca4f399a341e4aad1a805e796285a69a77361
hibestil <halilibrahimbestil@hotmail.com>
2016-05-07 19:16:42 +0300

    Binary Trees
    We extend the concept of linked data structures to structure containing nodes with more than one self-referenced field. A binary tree is made of nodes, where each node contains a "left" reference, a "right" reference, and a data element. The topmost node in the tree is called the root.
    
    Every node (excluding a root) in a tree is connected by a directed edge from exactly one other node. This node is called a parent. On the other hand, each node can be connected to arbitrary number of nodes, called children. Nodes with no children are called leaves, or external nodes. Nodes which are not leaves are called internal nodes. Nodes with the same parent are called siblings.
    	   
    
    More tree terminology:
    
        The depth of a node is the number of edges from the root to the node.
        The height of a node is the number of edges from the node to the deepest leaf.
        The height of a tree is a height of the root.
        A full binary tree.is a binary tree in which each node has exactly zero or two children.
        A complete binary tree is a binary tree, which is completely filled, with the possible exception of the bottom level, which is filled from left to right.

---
+++ binaryTree.c
@@ -0,0 +1,63 @@
+/*
+	@Author
+		Name : Halil İbrahim Bestil
+		Web-site : http://www.hibestil.com
+		E-Mail Adress : hibestil [at] gmail [.] com
+		School: Yildiz Technical University - Computer Engineering
+	@About
+		This program creates a simple Binary tree. 
+		
+	@Simple
+	There is a basic Binary tree 
+	
+	      (60)					<====== This line is tree root
+             /    \
+            /      \
+        (50)        (80)			<====== This are tree's branchs
+        /   \       /   \
+      (40) (55)   (75)  (90)			
+      / \   /\    /\	  /\
+    (0)(0) /  \	 (0)(0) (0)(0)		
+         (52)(57) 					<====== This are tree's leaves
+	      
+		
+		
+		
+*/
+
+#include <stdio.h>
+int main(){
+	/* 
+		@param eleman : gets element for to put elements to their location . 
+		@param N  :	Number of elements
+	*/
+	static int A[20];
+	int I,i,j,eleman,N;
+
+	printf("What is your Array lenght ? :\n");
+	scanf("%d",&N); // get number of elements	
+	printf("Enter elements of array : \n");
+		// In this part program gets elements . Then puts elements to their location  
+		for( i=0 ;i<N; i++){
+			I=1;
+			printf("	*What is %d. element? :\n	*** :",i+1);
+			scanf("%d",&eleman); // gets a new element 
+			while((I<=N)&&(A[I]!=0)){ // puts the element to its location  
+				if(eleman>A[I]) I=2*I+1;
+				else I=2*I;	
+			}
+			A[I]=eleman;
+			printf("	*****A[%d] = %d \n",I,A[I]);
+		}
+	
+	// Write Array
+	for(i= 1 ; i<=I;i++)
+		printf("%d , ",A[i]);
+	}	
+	
+	
+	getch();
+	return 0;
+
+	
+}
